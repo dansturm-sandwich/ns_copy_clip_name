67be95023f2bf989e4c62d6987c42292f901a28f
dansturm-sandwich <dan@sandwich.co>
2023-11-21 22:52:44 -0700

    modified to pull clip name plus version of clip

--- ns_copy_clip_name.py
+++ ns_copy_clip_name.py
@@ -5,7 +5,8 @@ import hiero
 def copy_clip_name():
     sequence = hiero.ui.activeSequence()
     selection = hiero.ui.getTimelineEditor(sequence).selection()
-    name_to_copy = selection[0].name()
+    name_with_version = selection[0].currentVersion()
+    name_to_copy = name_with_version.name()
 
     # Determine the platform
     platform = os.name
